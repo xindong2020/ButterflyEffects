7ceeed712a12a9bf933b7aa8967cea50e9d4ea76
fandong93 <641743220@qq.com>
2024-01-29 00:22:40 +0800

    commit butterfly effects files

--- background/snow/snow.js
+++ background/snow/snow.js
@@ -1,41 +1,42 @@
+//背景雪花飘落特效
 (function($){
-    $.fn.snow = function(options){
-    var $flake = $('<div id="snowbox" />').css({'position': 'absolute','z-index':'9999', 'top': '-50px'}).html('&#10052;'),
-    documentHeight  = $(document).height(),
-    documentWidth   = $(document).width(),
-    defaults = {
-        minSize     : 10,
-        maxSize     : 20,
-        newOn       : 1000,
-        flakeColor  : "#FFFFFF" /* 此处可以定义雪花颜色 */
-    },
-    options = $.extend({}, defaults, options);
-    var interval= setInterval( function(){
-    var startPositionLeft = Math.random() * documentWidth - 100,
-    startOpacity = 0.5 + Math.random(),
-    sizeFlake = options.minSize + Math.random() * options.maxSize,
-    endPositionTop = documentHeight - 200,
-    endPositionLeft = startPositionLeft - 500 + Math.random() * 500,
-    durationFall = documentHeight * 10 + Math.random() * 5000;
-    $flake.clone().appendTo('body').css({
-        left: startPositionLeft,
-        opacity: startOpacity,
-        'font-size': sizeFlake,
-        color: options.flakeColor
-    }).animate({
-        top: endPositionTop,
-        left: endPositionLeft,
-        opacity: 0.2
-    },durationFall,'linear',function(){
-        $(this).remove()
-    });
-    }, options.newOn);
+	$.fn.snow = function(options){
+	var $flake = $('<div id="snowbox" />').css({'position': 'absolute','z-index':'9999', 'top': '-50px'}).html('&#10052;'),
+	documentHeight 	= $(document).height(),
+	documentWidth	= $(document).width(),
+	defaults = {
+		minSize		: 10,
+		maxSize		: 20,
+		newOn		: 1000,
+		flakeColor	: "#FFFFFF" /* 此处可以定义雪花颜色 */
+	},
+	options	= $.extend({}, defaults, options);
+	var interval= setInterval( function(){
+	var startPositionLeft = Math.random() * documentWidth - 100,
+	startOpacity = 0.5 + Math.random(),
+	sizeFlake = options.minSize + Math.random() * options.maxSize,
+	endPositionTop = documentHeight - 10,	/* 此处可以定义雪花下落消失的位置 */
+	endPositionLeft = startPositionLeft - 500 + Math.random() * 500,
+	durationFall = documentHeight * 10 + Math.random() * 5000;
+	$flake.clone().appendTo('body').css({
+		left: startPositionLeft,
+		opacity: startOpacity,
+		'font-size': sizeFlake,
+		color: options.flakeColor
+	}).animate({
+		top: endPositionTop,
+		left: endPositionLeft,
+		opacity: 0.3
+	},durationFall,'linear',function(){
+		$(this).remove()
+	});
+	}, options.newOn);
     };
 })(jQuery);
 $(function(){
     $.fn.snow({ 
-        minSize: 5,		/* 定义雪花最小尺寸 */
-        maxSize: 20,	/* 定义雪花最大尺寸 */
-        newOn: 100		/* 定义密集程度，数字越小越密集 */
+	    minSize: 3,		/* 定义雪花最小尺寸 */
+	    maxSize: 20,	/* 定义雪花最大尺寸 */
+	    newOn: 50		/* 定义密集程度，数字越小越密集 */
     });
 });
