c3f84359fc9d039251950def8cd3d706b2907732
fandong93 <641743220@qq.com>
2024-01-28 22:18:13 +0800

    commit butterfly effects files

--- background/snow/snow.js
+++ background/snow/snow.js
@@ -7,7 +7,7 @@
         minSize     : 10,
         maxSize     : 20,
         newOn       : 1000,
-        flakeColor  : "#AFDAEF" /* 此处可以定义雪花颜色，若要白色可以改为#FFFFFF */
+        flakeColor  : "#FFFFFF" /* 此处可以定义雪花颜色 */
     },
     options = $.extend({}, defaults, options);
     var interval= setInterval( function(){
@@ -35,7 +35,7 @@
 $(function(){
     $.fn.snow({ 
         minSize: 5,		/* 定义雪花最小尺寸 */
-        maxSize: 50,	/* 定义雪花最大尺寸 */
-        newOn: 300		/* 定义密集程度，数字越小越密集 */
+        maxSize: 20,	/* 定义雪花最大尺寸 */
+        newOn: 100		/* 定义密集程度，数字越小越密集 */
     });
 });
